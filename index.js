df5b57e23d3e8ca316a9c93c8e911ba401ba8d47
Diego Isla <dislalopez@gmail.com>
2022-04-28 12:37:43 -0500

    Error handling

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 document.getElementById("inputfile").addEventListener("change", function () {
   var fr = new FileReader();
+
   fr.onload = function () {
-    //  document.getElementById("output").textContent = fr.result;
     let res = fr.result;
     let lines = res.split(/\r\n|\n/);
-    lines.forEach((element) => {
-      if (element !== "") {
-        a = getPay(element);
-        tag = document.createElement("p");
-        txt = document.createTextNode(
-          "The amount to pay " + a[0] + " is " + a[1].toString() + " USD \n"
-        );
-        tag.append(txt);
-        document.getElementById("output").append(tag);
+    for (let line of lines) {
+      if (line !== "") {
+        a = getPay(line);
+        if (a !== 0) {
+          tag = document.createElement("p");
+          txt = document.createTextNode(
+            "The amount to pay " + a[0] + " is " + a[1].toString() + " USD \n"
+          );
+          tag.append(txt);
+          document.getElementById("output").append(tag);
+        } else {
+          tag = document.createElement("p");
+          txt = document.createTextNode("Format error");
+          tag.append(txt);
+          document.getElementById("output").append(tag);
+          break;
+        }
       }
-    });
+    }
   };
 
   fr.readAsText(this.files[0]);
@@ -23,50 +31,54 @@ document.getElementById("inputfile").addEventListener("change", function () {
 // how to set up the calculation rules?
 
 function getPay(line) {
-  weekdays = ["MO", "TU", "WE", "TH", "FR"];
+  let weekdays = ["MO", "TU", "WE", "TH", "FR"];
   // get employee name
-  fs = line.split("=");
-  employeeName = fs[0];
-  // get days worked
-  days = fs[1].split(",");
-  pay = 0;
+  try {
+    let fs = line.split("=");
+    let employeeName = fs[0];
+    // get days worked
+    let days = fs[1].split(",");
+    let pay = 0;
 
-  days.forEach((element) => {
-    if (element !== "") {
-      day = element.charAt(0) + element.charAt(1);
-      times = element.replace(day, "").split("-");
-      t1 = times[0].split(":")[0];
-      m1 = times[0].split(":")[1];
-      t2 = times[1].split(":")[0];
-      m2 = times[1].split(":")[1];
+    days.forEach((element) => {
+      if (element !== "") {
+        let day = element.charAt(0) + element.charAt(1);
+        let times = element.replace(day, "").split("-");
+        let t1 = times[0].split(":")[0];
+        let m1 = times[0].split(":")[1];
+        let t2 = times[1].split(":")[0];
+        let m2 = times[1].split(":")[1];
 
-      if (t2 == 0) {
-        t2 = 24;
-      }
+        if (t2 == 0) {
+          t2 = 24;
+        }
 
-      if (weekdays.includes(day)) {
-        for (let i = t1; i < t2; i++) {
-          if (i >= 0 && i < 9) {
-            pay += 25;
-          } else if (i >= 9 && i < 18) {
-            pay += 15;
-          } else if (i >= 18 && i < 24) {
-            pay += 20;
+        if (weekdays.includes(day)) {
+          for (let i = t1; i < t2; i++) {
+            if (i >= 0 && i < 9) {
+              pay += 25;
+            } else if (i >= 9 && i < 18) {
+              pay += 15;
+            } else if (i >= 18 && i < 24) {
+              pay += 20;
+            }
+          }
+        } else {
+          for (let i = t1; i < t2; i++) {
+            if (i >= 0 && i < 9) {
+              pay += 30;
+            } else if (i >= 9 && i < 18) {
+              pay += 20;
+            } else if (i >= 18 && i < 24) {
+              pay += 25;
+            }
           }
         }
-      } else {
-        for (let i = t1; i < t2; i++) {
-          if (i >= 0 && i < 9) {
-            pay += 30;
-          } else if (i >= 9 && i < 18) {
-            pay += 20;
-          } else if (i >= 18 && i < 24) {
-            pay += 25;
-          }
-        }
       }
-    }
-  });
+    });
 
-  return [employeeName, pay];
+    return [employeeName, pay];
+  } catch (error) {
+    return 0;
+  }
 }
