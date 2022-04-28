5a26b56bdfeb9e243c847ee09f03d091e9cf3fdd
Diego Isla <dislalopez@gmail.com>
2022-04-27 19:16:51 -0500

    basic file reading and processing

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 document.getElementById("inputfile").addEventListener("change", function () {
   var fr = new FileReader();
   fr.onload = function () {
-    document.getElementById("output").textContent = fr.result;
+    //  document.getElementById("output").textContent = fr.result;
+    let res = fr.result;
+    let lines = res.split(/\r\n|\n/);
+    lines.forEach((element) => {
+      if (element !== "") {
+        a = getPay(element);
+        console.log(a);
+      }
+    });
   };
 
   fr.readAsText(this.files[0]);
 });
+
+function getPay(line) {
+  // get employee name
+  fs = line.split("=");
+  employeeName = fs[0];
+  days = fs[1].split(",");
+  console.log(days);
+  pay = 0;
+  return [employeeName, pay];
+}
