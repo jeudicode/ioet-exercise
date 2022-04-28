f893c8321f4d663984da51be6eeae079bc343e50
Diego Isla <dislalopez@gmail.com>
2022-04-28 03:32:14 -0500

    Salary calculation

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ document.getElementById("inputfile").addEventListener("change", function () {
     lines.forEach((element) => {
       if (element !== "") {
         a = getPay(element);
-        console.log(a);
+        tag = document.createElement("p");
+        txt = document.createTextNode(
+          "The amount to pay " + a[0] + " is " + a[1].toString() + "\n"
+        );
+        tag.append(txt);
+        document.getElementById("output").append(tag);
       }
     });
   };
@@ -15,12 +20,53 @@ document.getElementById("inputfile").addEventListener("change", function () {
   fr.readAsText(this.files[0]);
 });
 
+// how to set up the calculation rules?
+
 function getPay(line) {
+  weekdays = ["MO", "TU", "WE", "TH", "FR"];
   // get employee name
   fs = line.split("=");
   employeeName = fs[0];
+  // get days worked
   days = fs[1].split(",");
-  console.log(days);
   pay = 0;
+
+  days.forEach((element) => {
+    if (element !== "") {
+      day = element.charAt(0) + element.charAt(1);
+      times = element.replace(day, "").split("-");
+      t1 = times[0].split(":")[0];
+      m1 = times[0].split(":")[1];
+      t2 = times[1].split(":")[0];
+      m2 = times[1].split(":")[1];
+
+      if (t2 == 0) {
+        t2 = 24;
+      }
+
+      if (weekdays.includes(day)) {
+        for (let i = t1; i < t2; i++) {
+          if (i >= 0 && i < 9) {
+            pay += 25;
+          } else if (i >= 9 && i < 18) {
+            pay += 15;
+          } else if (i >= 18 && i < 24) {
+            pay += 20;
+          }
+        }
+      } else {
+        for (let i = t1; i < t2; i++) {
+          if (i >= 0 && i < 9) {
+            pay += 30;
+          } else if (i >= 9 && i < 18) {
+            pay += 20;
+          } else if (i >= 18 && i < 24) {
+            pay += 25;
+          }
+        }
+      }
+    }
+  });
+
   return [employeeName, pay];
 }
